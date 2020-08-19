b4079bfba64a40f5f64d9b34547cced1d722ba95
mike <mikemcfaddin@gmail.com>
2020-08-19 15:54:11 -0400

    added active column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_154819) do
+ActiveRecord::Schema.define(version: 2020_08_19_195217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_154819) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "active"
   end
 
 end
