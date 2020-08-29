8c3404384cc82ec36d2e9c8aa2644167a38aba30
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-08-29 14:07:42 -0500

    updated tables in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_193158) do
+ActiveRecord::Schema.define(version: 2020_08_29_173945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2020_08_28_193158) do
     t.string "title"
     t.text "text"
     t.integer "sort_order"
-    t.integer "boilerplate_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
