7f0dc671b14540f23c4bf35cd1ee13219f08b6c0
mike <mikemcfaddin@gmail.com>
2020-10-20 14:23:43 -0400

    adjusted cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'localhost:3000'
+    origins "localhost:8080", "localhost:3001"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
