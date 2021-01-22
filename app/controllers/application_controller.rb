bb5c43ceaa36e69ae1b064c15439a060ec1b4f9f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-01-21 21:57:48 -0600

    more work on cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+    include ::ActionController::Cookies
+    # include ::ActionDispatch::Cookies::CookieJars
+
+    protect_from_forgery with: :null_session
+    # protect_from_forgery with: :exception
 
   def current_user
-    auth_headers = request.headers["Authorization"]
-    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
-      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+    jwt = cookies.signed[:jwt]
+    p cookies[:request]
+    if jwt
+      token = jwt
       begin
         decoded_token = JWT.decode(
           token,
@@ -20,6 +25,19 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+
+  JWT_SECRET = Rails.application.secrets.secret_key_base
+
+  def decode_jwt(token)
+    begin
+    body = JWT.decode(token, JWT_SECRET)
+      if body then HashWithIndifferentAccess.new body[0] else return false end
+    rescue JWT::ExpiredSignature, JWT::VerificationError => e
+      return false
+    rescue JWT::DecodeError, JWT::VerificationError => e
+      return false
+    end
+  end
 
   def authenticate_user
     unless current_user
