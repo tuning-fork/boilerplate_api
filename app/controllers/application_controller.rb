45a55b541283ef2f4710e8a7db55d9b60d0df32a
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-01-22 09:32:00 -0600

    reconciled stashed branches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,101 @@
 class ApplicationController < ActionController::Base
-    include ::ActionController::Cookies
+    # include ::ActionController::Cookies
     # include ::ActionDispatch::Cookies::CookieJars
 
     protect_from_forgery with: :null_session
     # protect_from_forgery with: :exception
 
-  def current_user
-    jwt = cookies.signed[:jwt]
-    p cookies[:request]
-    if jwt
-      token = jwt
-      begin
-        decoded_token = JWT.decode(
-          token,
-          Rails.application.credentials.fetch(:secret_key_base),
-          true,
-          { algorithm: "HS256" }
-        )
-        User.find_by(id: decoded_token[0]["user_id"])
-      rescue JWT::ExpiredSignature
-        nil
+    def current_user
+      auth_headers = request.headers["Authorization"]
+      if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+        token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+        begin
+          decoded_token = JWT.decode(
+            token,
+            Rails.application.credentials.fetch(:secret_key_base),
+            true,
+            { algorithm: "HS256" }
+          )
+          User.find_by(id: decoded_token[0]["user_id"])
+        rescue JWT::ExpiredSignature
+          nil
+        end
       end
     end
-  end
 
-  helper_method :current_user
-
-  JWT_SECRET = Rails.application.secrets.secret_key_base
-
-  def decode_jwt(token)
-    begin
-    body = JWT.decode(token, JWT_SECRET)
-      if body then HashWithIndifferentAccess.new body[0] else return false end
-    rescue JWT::ExpiredSignature, JWT::VerificationError => e
-      return false
-    rescue JWT::DecodeError, JWT::VerificationError => e
-      return false
+    helper_method :current_user
+    
+    def authenticate_user
+      unless current_user
+        render json: {}, status: :unauthorized
+      end
     end
+
+  #   def current_user
+  #     jwt = cookies.signed[:jwt]
+  #     if jwt
+  #       token = jwt
+  #       begin
+  #         decoded_token = JWT.decode(
+  #           token,
+  #           Rails.application.credentials.fetch(:secret_key_base),
+  #           true,
+  #           { algorithm: "HS256" }
+  #         )
+  #         User.find_by(id: decoded_token[0]["user_id"])
+  #       rescue JWT::ExpiredSignature
+  #         nil
+  #       end
+  #     end
+  #   end
+
+  #   helper_method :current_user
+
+  #   JWT_SECRET = Rails.application.secrets.secret_key_base
+
+  #   def decode_jwt(token)
+  #     begin
+  #     body = JWT.decode(token, JWT_SECRET)
+  #       if body then HashWithIndifferentAccess.new body[0] else return false end
+  #     rescue JWT::ExpiredSignature, JWT::VerificationError => e
+  #       return false
+  #     rescue JWT::DecodeError, JWT::VerificationError => e
+  #       return false
+  #     end
+  #   end
+
+  #   def authenticate_user
+  #     jwt = cookies.signed[:jwt]
+  #     decode_jwt(jwt)
+  #     unless jwt
+  #       render json: {}, status: :unauthorized
+  #     end
+  #   end
+
   end
 
-  def authenticate_user
-    unless current_user
-      render json: {}, status: :unauthorized
-    end
-  end
-end
+# pre-cookie state
+
+# def current_user
+#   auth_headers = request.headers["Authorization"]
+#   if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+#     token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+#     begin
+#       decoded_token = JWT.decode(
+#         token,
+#         Rails.application.credentials.fetch(:secret_key_base),
+#         true,
+#         { algorithm: "HS256" }
+#       )
+#       User.find_by(id: decoded_token[0]["user_id"])
+#     rescue JWT::ExpiredSignature
+#       nil
+#     end
+#   end
+# end
+# helper_method :current_user
+# def authenticate_user
+#   unless current_user
+#     render json: {}, status: :unauthorized
+#   end
+# end
