bda6141891432c9b11adc7bb6eff568e6948a0b7
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-10-21 17:44:03 -0500

    updated graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -44,7 +44,6 @@ class GraphqlController < ApplicationController
   def handle_error_in_development(e)
     logger.error e.message
     logger.error e.backtrace.join("\n")
-
     render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
   end
 end
