f96b39b7f5098762b34f4184b22da073b259ee31
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-02-25 15:38:12 -0600

    Simplify fetching, creation, saving, & deleting reports

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -2,31 +2,25 @@ class Api::ReportsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @reports = Report.all
-
-    @reports = @reports.order(id: :asc)
+    @reports = Report
+      .where(grant_id: params[:grant_id])
+      .order(id: :asc)
 
     render "index.json.jb"
   end
 
   def create
-    @report = Report.new(
+    @report = Report.create!(
       grant_id: params[:grant_id],
       title: params[:title],
       deadline: params[:deadline],
       submitted: params[:submitted],
-      # archived: false
     )
-    if @report.save
-      render "show.json.jb"
-    else
-      render json: { errors: @report.errors.messages }, status: :unprocessable_entity
-    end
+    render "show.json.jb", status: 201
   end
 
   def show
     @report = Report.find(params[:id])
-    # render json: @report, include: [:report_sections, :grant]
     render 'show.json.jb'
   end
 
@@ -38,17 +32,15 @@ class Api::ReportsController < ApplicationController
     @report.deadline = params[:deadline] || @report.deadline
     @report.submitted = params[:submitted].nil? || @report.submitted
     @report.archived = params[:archived].nil? || @report.archived
+    @report.save!
 
-    if @report.save
-      render "show.json.jb"
-    else
-      render json: { errors: @report.errors.messages }, status: :unprocessable_entity
-    end
+    render "show.json.jb"
   end
 
   def destroy
-    report = Report.find(params[:id])
-    report.destroy
-    render json: { message: "Report successfully destroyed." }
+    @report = Report.find(params[:id])
+    @report.destroy!
+
+    render "show.json.jb"
   end
 end
