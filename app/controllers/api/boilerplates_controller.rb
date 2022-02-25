aa708665e2ab5438764ab079f918585ad7e5336d
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-02-25 14:09:06 -0600

    Simplify creating, updating, & destroying handling of boilerplates

--- app/controllers/api/boilerplates_controller.rb
+++ app/controllers/api/boilerplates_controller.rb
@@ -3,28 +3,20 @@ class Api::BoilerplatesController < ApplicationController
   before_action :ensure_boilerplate_exists, only: [:show, :update, :destroy]
 
   def index
-    # @boilerplates = Boilerplate.all
     @boilerplates = Boilerplate.where(organization_id: params[:organization_id])
-
-    # @boilerplates = @boilerplates.order(:title)
 
     render "index.json.jb"
   end
 
   def create
-    @boilerplate = Boilerplate.new(
+    @boilerplate = Boilerplate.create!(
       organization_id: params[:organization_id],
       category_id: params[:category_id],
       title: params[:title],
       text: params[:text],
       wordcount: params[:wordcount],
-      # archived: false
     )
-    if @boilerplate.save
-      render "show.json.jb"
-    else
-      render json: { errors: @boilerplate.errors.full_messages }, status: :unprocessable_entity
-    end
+    render "show.json.jb", status: 201
   end
 
   def show
@@ -38,17 +30,14 @@ class Api::BoilerplatesController < ApplicationController
     @boilerplate.text = params[:text] || @boilerplate.text
     @boilerplate.wordcount = params[:wordcount] || @boilerplate.wordcount
     @boilerplate.archived = params[:archived].nil? ? @boilerplate.archived : params[:archived]
+    @boilerplate.save!
 
-    if @boilerplate.save
-      render "show.json.jb"
-    else
-      render json: { errors: @boilerplate.errors.messages }, status: :unprocessable_entity
-    end
+    render "show.json.jb"
   end
 
   def destroy
-    @boilerplate.destroy
-    render json: { message: "Boilerplate successfully destroyed" }
+    @boilerplate.destroy!
+    render "show.json.jb"
   end
 
   private
