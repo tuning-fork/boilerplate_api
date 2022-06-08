12a6b80be4ad22a33667804bd63cd6196c7b378a
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-08 07:27:41 -0500

    added routes for reviewers and refs for resources

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
         resources :reports do
           resources :report_sections
         end
+        resources :reviewers
       end
     end
 
@@ -26,3 +27,34 @@ Rails.application.routes.draw do
     get "/organizations/:organization_id/reports-finalize/:id" => "reports#show"
   end
 end
+
+# Reference guide for resources routes:
+# https://guides.rubyonrails.org/routing.html
+# ordinary or non-nested routes using resources:
+# resources :photos
+# creates seven different routes in your application, all mapping to the Photos controller:
+
+# HTTP Verb	Path	Controller#Action	Used for
+# GET	/photos	photos#index	display a list of all photos
+# GET	/photos/new	photos#new	return an HTML form for creating a new photo
+# POST	/photos	photos#create	create a new photo
+# GET	/photos/:id	photos#show	display a specific photo
+# GET	/photos/:id/edit	photos#edit	return an HTML form for editing a photo
+# PATCH/PUT	/photos/:id	photos#update	update a specific photo
+# DELETE	/photos/:id	photos#destroy	delete a specific photo
+
+# nested routes using resources:
+# resources :magazines do
+#   resources :ads
+# end
+
+# In addition to the routes for magazines, this declaration will also route ads to an AdsController. The ad URLs require a magazine:
+
+# HTTP Verb	Path	Controller#Action	Used for
+# GET	/magazines/:magazine_id/ads	ads#index	display a list of all ads for a specific magazine
+# GET	/magazines/:magazine_id/ads/new	ads#new	return an HTML form for creating a new ad belonging to a specific magazine
+# POST	/magazines/:magazine_id/ads	ads#create	create a new ad belonging to a specific magazine
+# GET	/magazines/:magazine_id/ads/:id	ads#show	display a specific ad belonging to a specific magazine
+# GET	/magazines/:magazine_id/ads/:id/edit	ads#edit	return an HTML form for editing an ad belonging to a specific magazine
+# PATCH/PUT	/magazines/:magazine_id/ads/:id	ads#update	update a specific ad belonging to a specific magazine
+# DELETE	/magazines/:magazine_id/ads/:id	ads#destroy	delete a specific ad belonging to a specific magazine
