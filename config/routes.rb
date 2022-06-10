8d547e71193643f9b52d789f77a4ca06e49f1f36
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-10 08:32:34 -0500

    added post route for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     post '/forgot_password' => "passwords#forgot"
     post '/reset_password' => "passwords#reset"
 
+    post '/contact_us' => "contact#create"
+
     resources :users, only: [:create, :update]
 
     resources :organizations do
