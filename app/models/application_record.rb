aea6265850329e3cefadf0079075a08dc1ffa12c
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-16 08:02:32 -0500

    shelved unnecessary string validation method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -16,4 +16,12 @@ class ApplicationRecord < ActiveRecord::Base
     text_score
   end 
 
+  #unused method for validating all (or any specified) values in a form input are strings
+  #kept for future reference
+  # validates_each :name, :email, :title, :organization_name, :message, allow_blank: true do |record, attribute, value|
+  #   unless value.is_a? String
+  #       record.errors[attribute] << (options[:message] || "#{value} is not a string")
+  #   end
+  # end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,6 @@
 class Contact < ApplicationRecord
     validates :name, presence: true, length: { minimum: 2 }
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :message, presence: true, length: { minimum: 5 }
-    
-    validates_each :name, :email, :title, :organization_name, :message, allow_blank: true do |record, attr, value|
-        unless value.is_a? String
-            record.errors[attribute] << (options[:message] || "#{value} is not a string")
-        end
-    end
 end
