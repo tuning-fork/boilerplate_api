e5cca0889e081baec903585551fc752e5e733e52
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-17 13:52:51 -0500

    updated model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,10 +5,7 @@ class Contact < ApplicationRecord
     validates :message, presence: true, length: { minimum: 5 }
 
     def send_contact_email(contact_submission)
-        # self.password_reset_token = generate_base64_token
-        # self.password_reset_sent_at = Time.zone.now
-        # save!
         ContactMailer.contact_submission(contact_submission).deliver_now
         ContactMailer.contact_confirmation(contact_submission).deliver_now
-      end
+    end
 end
