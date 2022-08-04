f93a863791dbc557bb604658bca1320e0ef62ede
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-08-04 18:46:51 -0500

    added validations for contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,9 @@
 class Contact < ApplicationRecord
-    validates :name, presence: true, length: { minimum: 2 }
-    validates :email, presence: true
+    validates :name, presence: true, length: { minimum: 2, minimum: 100 }
+    validates :email, presence: true, length: { maximum: 50 }
+    validates :organization_name, length: { maximum: 50 }
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates :message, presence: true, length: { minimum: 5 }
+    validates :message, presence: true, length: { minimum: 5, maximum: 500 }
 
     def send_contact_email(contact_submission)
         ContactMailer.contact_submission(contact_submission).deliver_now
