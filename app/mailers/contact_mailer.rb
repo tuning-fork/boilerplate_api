6fef704fefc7183a62f01b1e61462d4a2bee339e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-08-07 16:26:45 -0500

    added environment variables to contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
     def contact_submission(contact_submission)
       @contact = contact_submission
-      mail to: "boilerplate.app.test@gmail.com", subject: "New Contact Submission: #{@contact.name} at #{@contact.organization_name}"
+      mail to: ENV.fetch("CONTACT_SUBMISSION_ADMIN_EMAIL"), subject: "New Contact Submission: #{@contact.name} at #{@contact.organization_name}"
     end
     
     def contact_confirmation(contact_submission)
