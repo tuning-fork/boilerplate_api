e27a4ed2e5dafbb5373047fe5d7ac6992e0f134d
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-08-07 16:20:49 -0500

    fixed validation typo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates :name, presence: true, length: { minimum: 2, minimum: 100 }
+    validates :name, presence: true, length: { minimum: 2, maximum: 100 }
     validates :email, presence: true, length: { maximum: 50 }
     validates :organization_name, length: { maximum: 50 }
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
