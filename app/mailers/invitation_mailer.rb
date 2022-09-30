4ca2aa202015775912dafcf68abcb43ad9b50267
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2022-09-30 11:33:14 -0500

    Add invitation_accepted mailer
    Also fixes a typo with the build_accept_link method
    
    Co-authored-by: Jess-White <Jess-White@users.noreply.github.com>

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,4 +6,10 @@ class InvitationMailer < ApplicationMailer
     mail to: @invitation.email,
          subject: "You've been invited to join #{@invitation.organization.name} on Boilerplate"
   end
+
+  def invitation_accepted
+    @invitation = params[:invitation]
+    mail to: @invitation.organization.admins.pluck(:email),
+         subject: "New user added to your organization #{@invitation.organization.name} on Boilerplate"
+  end
 end
--- app/mailers/previews/invitation_mailer_preview.rb
+++ app/mailers/previews/invitation_mailer_preview.rb
@@ -5,4 +5,8 @@ class InvitationMailerPreview < ActionMailer::Preview
   def invite
     InvitationMailer.with(invitation: Invitation.last).invite
   end
+
+  def invitation_accepted
+    InvitationMailer.with(invitation: Invitation.last).invitation_accepted
+  end
 end
